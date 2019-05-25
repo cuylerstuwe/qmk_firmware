a64219e2847459b5a66bbb2b9d5ce6422edefcd5
Cuyler Stuwe <salembeats@gmail.com>
2019-05-25 11:11:52 -0700

    centered symbols on planck symbol number layer

--- keyboards/planck/keymaps/salembeats/keymap.c
+++ keyboards/planck/keymaps/salembeats/keymap.c
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [SYMBOL_LAYER] = LAYOUT_planck_grid(
-  KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL ,
+  _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
