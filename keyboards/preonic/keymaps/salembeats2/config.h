7a22e1a183f60560a2f7e301e8756c0e25ada9bf
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2024-11-03 11:42:59 -0800

    make the preonic 1000hz, add a csgo layer

--- keyboards/preonic/keymaps/salembeats2/config.h
+++ keyboards/preonic/keymaps/salembeats2/config.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define USB_POLLING_INTERVAL_MS 1  // Sets polling to 1000 Hz globally
+
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
--- keyboards/preonic/keymaps/salembeats2/keymap.c
+++ keyboards/preonic/keymaps/salembeats2/keymap.c
@@ -17,6 +17,29 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+// Define the custom QWERTY tone array
+float custom_qwerty_sound[][2] = {
+    { NOTE_C4, 20 }, { NOTE_E4, 20 }
+};
+
+// Define the CSGO theme tone array
+float csgo_theme[][2] = {
+    { NOTE_G4, 16 }, { NOTE_A4, 16 }, { NOTE_B4, 16 }, { NOTE_D5, 16 },
+    { NOTE_E5, 16 }, { NOTE_D5, 16 }, { NOTE_B4, 16 }, { NOTE_A4, 16 }
+};
+
+void play_qwerty_tone(void) {
+    PLAY_SONG(custom_qwerty_sound);
+}
+
+void play_csgo_tone(void) {
+    PLAY_SONG(csgo_theme);
+}
+
+// Variables for tracking triple-tap on F1
+static uint16_t f1_triple_tap_timer;
+static uint8_t f1_tap_count = 0;
+
 enum preonic_layers {
   _QWERTY,
   _LEFT_LOWER,
@@ -27,7 +50,8 @@ enum preonic_layers {
   _DOUBLE_RAISE,
   NAV_LAYER,
   NUMPAD_LAYER,
-  SYMBOL_LAYER
+  SYMBOL_LAYER,
+  COUNTER_STRIKE_LAYER
 };
 
 enum preonic_keycodes {
@@ -50,6 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CTL_T(KC_ESC),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,        KC_L,    LT(NAV_LAYER, KC_SCLN), CTL_T(KC_ENT),
     KC_LSFT,                  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM,     KC_DOT,  KC_SLSH,                KC_RSFT,
     SYMBOL_MODE,              LEFT_RAISE,       KC_LALT, KC_LGUI, LEFT_LOWER, KC_SPC,  KC_SPC,  RIGHT_LOWER, KC_RGUI, KC_RALT, RIGHT_RAISE,   SYMBOL_MODE
+),
+
+[COUNTER_STRIKE_LAYER] = LAYOUT_preonic_grid(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, ___x___, ___x___, ___x___, KC_DOWN, KC_SPC,  KC_SPC,  ___x___, ___x___, ___x___, ___x___,  KC_RCTL
 ),
 
 [_LEFT_LOWER] = LAYOUT_preonic_grid(
@@ -134,6 +166,44 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case KC_F1:
+    case KC_ESC:
+        if (record->event.pressed) {
+            // Check if the timer has expired
+            if (timer_elapsed(f1_triple_tap_timer) > TAPPING_TERM) {
+                // Reset tap count if timer has expired
+                f1_tap_count = 0;
+            }
+
+            // Start or restart the timer
+            f1_triple_tap_timer = timer_read();
+
+            // Increment the tap count
+            f1_tap_count++;
+
+            // Check if it's the third tap
+            if (f1_tap_count == 3) {
+                // Reset tap count
+                f1_tap_count = 0;
+
+                // Toggle layers based on the current layer
+                if (layer_state_is(COUNTER_STRIKE_LAYER)) {
+                    // Switch to QWERTY if on Counter Strike
+                    // layer_move(_QWERTY);
+                    layer_move(_QWERTY);
+                    play_qwerty_tone();
+                } else {
+                    // Switch to Counter Strike if on QWERTY or any other layer
+                    layer_on(COUNTER_STRIKE_LAYER);
+                    play_csgo_tone();
+                }
+
+                return false; // Skip further processing of this keypress
+            }
+
+            return true;
+        }
+        break;
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
